1f050e73eb3cc06f5ba9d929605bf9875139e563
@Alexandre.Yoshitake <alexandre.yoshitake@dpu.def.br>
2023-11-01 19:06:04 -0300

    Cadastro de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'cpf',
+        'data_nascimento',
+        'celular',
         'email',
         'password',
+        'permission',
     ];
 
     /**
